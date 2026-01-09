c1139c757257d05fdf0e66202a1f459d6e270194
Jeevietha <jeevietha11@gmail.com>
2026-01-09 10:31:38 +0530

    q4
    Implement function to find the highest occurring character in a string.

---
+++ ASSIGNMENT 1/high_occurance_string.py
@@ -0,0 +1,18 @@
+def highest_char(s):
+    max_char = s[0]
+    max_count = 0
+
+    for ch in s:
+        count = s.count(ch)
+        if count > max_count:
+            max_count = count
+            max_char = ch
+
+    return max_char, max_count
+
+
+s = input("Enter string: ")
+ch, cnt = highest_char(s)
+print("Character:", ch)
+print("Count:", cnt)
+
